3aee31a6bce4c6b70b59403e790d00cad1573b4b
PAVincius <vinicius.pratesaraujo@gmail.com>
2025-03-09 20:16:53 -0300

    feat: add google login

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Cadastro from "./pages/Cadastro.tsx";
 import Account from "./pages/Account/Account.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 const router = createBrowserRouter([
   {
@@ -27,7 +28,9 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <GoogleOAuthProvider clientId='712065091138-0iaa0qpolcm1646nmnd91thctaqinv9v.apps.googleusercontent.com'>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </GoogleOAuthProvider>
 );
