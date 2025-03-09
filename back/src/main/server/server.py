a9d40240f928381a74236aab081cf29877ed23cc
PAVincius <vinicius.pratesaraujo@gmail.com>
2025-03-09 20:36:28 -0300

    feat: add back-cadastro

--- back/src/main/server/server.py
+++ back/src/main/server/server.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
 from src.main.routes.user import user_route_bp
+from src.main.routes.send_email import email_route_bp
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../../services/templates')
 CORS(app)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'devfoodsender@gmail.com'
+app.config['MAIL_PASSWORD'] = 'hmmw mwim ofij jvnd'
+
 app.register_blueprint(user_route_bp)
+app.register_blueprint(email_route_bp)
---
+++ back/src/services/email_sender.py
@@ -0,0 +1,47 @@
+from flask import render_template
+from flask_mail import Mail, Message
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
+from random import randint
+import os
+
+class EmailSender:
+    def __init__(self):
+        from ..main.server.server import app
+        self.mail = Mail(app)
+        self.template_path = os.path.join(os.path.dirname(__file__), 'email_template.html')
+
+    def send_email(self, http_request: HttpRequest) -> HttpResponse:
+        destination_email = self.__get_email(http_request)
+        verification_code = self.__generate_code()
+        message = render_template("email_message_template.html", verification_code=verification_code)
+        msg = Message(
+            subject="Seu código de verificação chegou! 🔐 ",
+            sender="devfoodsender@gmail.com",
+            recipients=[destination_email],
+            html=message
+        )
+        self.mail.send(msg)
+        return self.__format_response(verification_code)
+        
+    def __get_email(self, http_request: HttpRequest) -> str:
+        return http_request.body["data"]["email"]
+    
+    def __generate_code(self) -> int:
+        return randint(100000, 999999)
+    
+    def __load_template(self):
+        with open(self.template_path, "r", encoding="utf-8") as file:
+            return file.read()
+    
+    def __format_response(self, verification_code: int) -> HttpResponse:
+        return HttpResponse(
+            body= {
+                "data": {
+                    "attributes":{
+                        "verification_code": verification_code
+                    }
+                }
+            },
+            status_code=200
+        )
