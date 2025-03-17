6edfae323b8d715d27ef7c627940bc9ed9f26594
Guilherme1608 <83888126+Guilherme1608@users.noreply.github.com>
2025-03-17 15:14:18 -0300

    feat: Criar Componente Header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import Menu from "./componentes/Menu";
 import Cadastro from "./pages/Cadastro";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen w-full m-auto mt-5">
-      <h1 className="text-3xl font-bold text-center m-2">
-      Falta pouco para matar sua fome!
-      </h1>
-      {/* {profile ? (
-                <div>
-                    <img src={profile.picture} alt="user image" />
-                    <h3>User Logged in</h3>
-                    <p>Name: {profile.name}</p>
-                    <p>Email Address: {profile.email}</p>
-                    <br />
-                    <br />
-                    <button onClick={logOut}>Log out</button>
-                </div>
-            ) : ( */}
-              <Cadastro />
-            {/* )} */}
-      <button>Sign in with Google 🚀 </button>
-    </div>
+    <>
+      <Menu />
+
+      <div className="flex flex-col items-center justify-center min-h-screen w-full m-auto mt-5">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1 className="text-3xl font-bold text-center m-2">
+                  Falta pouco para matar sua fome!
+                </h1>
+                <Cadastro />
+                <button>Sign in with Google 🚀 </button>
+              </>
+            }
+          />
+          <Route path="/cardapio" element={<h1>Cardápio</h1>} />
+          <Route path="/pedidos" element={<h1>Meus Pedidos</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+        </Routes>
+      </div>
+    </>
   );
 }
+
 export default App;
---
+++ front/src/componentes/Menu.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import iFoodLogo from "../assets/ifood.png";
+import Button from "./Button";
+
+export default function Menu() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado de autenticação
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Lógica para logout do usuário
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <header className="bg-gray-100 shadow-md fixed w-full px-20 top-0 left-0 right-0 z-50 mx-0">
+      <div className="flex justify-between items-center p-5 mx-10">
+        {/* Logo */}
+        <Link to="/">
+          <img src={iFoodLogo} alt="iFood Logo" className="h-15" />
+        </Link>
+
+        {/* Botões */}
+        <div className="flex gap-14">
+          <Button
+            variant="plain"
+            onClick={() => navigate("/pedido")}
+            className="!w-40 !py-2 cursor-pointer"
+          >
+            Faça seu pedido
+          </Button>
+          {isAuthenticated ? (
+            <Button
+              onClick={handleLogout}
+              className="!w-25 !py-2 cursor-pointer"
+            >
+              Sair
+            </Button>
+          ) : (
+            <Button
+              onClick={() => navigate("/login")}
+              className="!w-25 !py-2 cursor-pointer"
+            >
+              Entrar
+            </Button>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
