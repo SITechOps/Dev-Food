d6465e51c1fe4246c62f69f5567f91ffae0c0ff1
RodrigoMazucato <rodrigolopes.sp2005@gmail.com>
2025-03-19 14:45:57 -0300

    chore: atualizar back-end com as últimas alterações da develop

--- back/src/model/entities/endereco.py
+++ back/src/model/entities/endereco.py
@@ -1,5 +1,5 @@
 from src.model.configs.base import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint
 
 class Endereco(Base):
     __tablename__ = "Endereco"
@@ -12,4 +12,19 @@ class Endereco(Base):
     numero = Column(Integer)
     complemento = Column(String(10), nullable=True)
     tipo = Column(String(20))
-    id_usuario = Column(Integer, ForeignKey("User.id"))
+    id_usuario = Column(Integer, ForeignKey("Usuario.id", ondelete="CASCADE"))
+
+    __table_args__ = (UniqueConstraint('tipo', 'id_usuario'),)
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "logradouro": self.logradouro,
+            "bairro": self.bairro,
+            "cidade": self.cidade,
+            "estado": self.estado,
+            "pais": self.pais,
+            "numero": self.numero,
+            "complemento": self.complemento,
+            "tipo": self.tipo,
+        }
--- back/src/model/repositories/enderecos_repository.py
+++ back/src/model/repositories/enderecos_repository.py
@@ -1,50 +1,32 @@
 from src.model.configs.connection import DBConnectionHandler
 from src.model.entities.endereco import Endereco
 from .interfaces.ienderecos_repository import IEnderecosRepository
+from src.main.handlers.custom_exceptions import AddressNotFound
 
 class EnderecosRepository(IEnderecosRepository):
 
-    def create_endereco(self, logradouro: str, bairro: str, cidade: str, estado: str, pais: str, numero: int, complemento: str, tipo: str, id_usuario: int) -> None:
+    def create_endereco(self, id_usuario: int, endereco: dict) -> None:
         with DBConnectionHandler() as db:
             try:
                 new_endereco = Endereco(
-                    logradouro=logradouro,
-                    bairro=bairro,
-                    cidade=cidade,
-                    estado=estado,
-                    pais=pais,
-                    numero=numero,
-                    complemento=complemento,
-                    tipo=tipo,
-                    id_usuario=id_usuario
+                    id_usuario=id_usuario,
+                    logradouro=endereco.get("logradouro"),
+                    bairro=endereco.get("bairro"),
+                    cidade=endereco.get("cidade"),
+                    estado=endereco.get("estado"),
+                    pais=endereco.get("pais"),
+                    numero=endereco.get("numero"),
+                    complemento=endereco.get("complemento"),
+                    tipo=endereco.get("tipo")
                 )
                 db.session.add(new_endereco)
                 db.session.commit()
-                return new_endereco.id
             except Exception as exception:
                 db.session.rollback()
                 raise exception
 
-    def get_endereco_by_id(self, endereco_id: int) -> Endereco:
-        with DBConnectionHandler() as db:
-            endereco = (
-                db.session
-                .query(Endereco)
-                .filter(Endereco.id == endereco_id)
-                .one_or_none()
-            )
-            return endereco
 
-    def list_enderecos(self) -> list[Endereco]:
-        with DBConnectionHandler() as db:
-            enderecos = (
-                db.session
-                .query(Endereco)
-                .all()
-            )
-            return enderecos
-
-    def get_endereco_by_usuario(self, id_usuario: int) -> list[Endereco]:
+    def get_all_enderecos_by_user(self, id_usuario: int) -> list[Endereco]:
         with DBConnectionHandler() as db:
             enderecos = (
                 db.session
@@ -53,33 +35,45 @@ class EnderecosRepository(IEnderecosRepository):
                 .all()
             )
             return enderecos
+        
 
-    def update_endereco(self, endereco_id: int, logradouro: str, bairro: str, cidade: str, estado: str, pais: str, numero: int, complemento: str, tipo: str) -> None:
+    def __get_endereco_by_id(self, id_endereco: int) -> Endereco | None:
+        with DBConnectionHandler() as db:
+            enderecos = (
+                db.session
+                .query(Endereco)
+                .filter(Endereco.id == id_endereco)
+                .one_or_none()
+            )
+            if not enderecos:
+                raise AddressNotFound()
+            return enderecos
+
+
+    def update_endereco(self, id_endereco: int, info: Endereco) -> None:
         with DBConnectionHandler() as db:
             try:
-                endereco = self.get_endereco_by_id(endereco_id)
-                if not endereco:
-                    raise Exception("Endereco não encontrado!")
-                endereco.logradouro = logradouro
-                endereco.bairro = bairro
-                endereco.cidade = cidade
-                endereco.estado = estado
-                endereco.pais = pais
-                endereco.numero = numero
-                endereco.complemento = complemento
-                endereco.tipo = tipo
+                endereco = self.__get_endereco_by_id(id_endereco)              
+                endereco.logradouro = info.get("logradouro")
+                endereco.bairro = info.get("bairro")
+                endereco.cidade = info.get("cidade")
+                endereco.estado = info.get("estado")
+                endereco.pais = info.get("pais")
+                endereco.numero = info.get("numero")
+                endereco.complemento = info.get("complemento")
+                endereco.tipo = info.get("tipo")
+
                 db.session.add(endereco)
                 db.session.commit()
             except Exception as exception:
                 db.session.rollback()
                 raise exception
 
-    def delete_endereco(self, endereco_id: int) -> None:
+
+    def delete_endereco(self, id_endereco: int) -> None:
         with DBConnectionHandler() as db:
             try:
-                endereco = self.get_endereco_by_id(endereco_id)
-                if not endereco:
-                    raise Exception("Endereco not found!")
+                endereco = self.__get_endereco_by_id(id_endereco)
                 db.session.delete(endereco)
                 db.session.commit()
             except Exception as exception:
