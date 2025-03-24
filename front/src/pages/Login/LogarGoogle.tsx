c2761b8e5574ed7f90f4e9e0ad4a145bf925b27f
Gustavo <gustrpaz@gmail.com>
2025-03-24 19:34:34 -0300

    feat: Inclusão do token no cadastro normal

--- front/src/pages/Login/LogarGoogle.tsx
+++ front/src/pages/Login/LogarGoogle.tsx
@@ -6,7 +6,6 @@ import { useState, useEffect } from "react";
 import { useGoogleLogin, TokenResponse } from "@react-oauth/google";
 import Button from "../../componentes/Button";
 
-
 export default function LogarGoogle() {
   const navigate = useNavigate();
   const { setUserLogged } = useAuth();
@@ -14,39 +13,48 @@ export default function LogarGoogle() {
 
   const loginGoogle = useGoogleLogin({
     onSuccess: (codeResponse) => setUser(codeResponse),
-    onError: (error) => console.log('Login Failed:', error)
+    onError: (error) => console.log("Login Failed:", error),
   });
 
   useEffect(() => {
     if (!user) return;
 
     axios
-      .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-        headers: {
-          Authorization: `Bearer ${user.access_token}`,
-          Accept: "application/json",
+      .get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+            Accept: "application/json",
+          },
         },
-      })
+      )
       .then(async (res) => {
         setUserLogged(res.data);
 
         const { data } = await api.get("/user");
         const usuarios = data?.data?.attributes || [];
 
-        const usuarioEncontrado = usuarios.find((usuario: any) => usuario.email === res.data.email);
+        const usuarioEncontrado = usuarios.find(
+          (usuario: any) => usuario.email === res.data.email,
+        );
 
         usuarioEncontrado
           ? navigate("/account", { state: { profile: res.data } })
-          : alert("Você não tem e-mail cadastrado no sistema. Faça seu cadastro.");
+          : alert(
+              "Você não tem e-mail cadastrado no sistema. Faça seu cadastro.",
+            );
       })
-      .catch((err) => console.error("Erro ao buscar informações do Google:", err));
+      .catch((err) =>
+        console.error("Erro ao buscar informações do Google:", err),
+      );
   }, [user]);
 
   return (
     <Button
       color="secondary"
       onClick={() => loginGoogle()}
-      className="justify-center items-center flex gap-4"
+      className="flex items-center justify-center gap-4"
     >
       <img src="img/google.svg" alt="" className="size-4" />
       Fazer login com o Google
