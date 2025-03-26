7ad820c5e4e7f6b4184d6544e889d7b4363bf5ac
RodrigoMazucato <rodrigolopes.sp2005@gmail.com>
2025-03-26 00:50:40 -0300

    refactor: Alterar tipo de ID para Char

--- back/src/model/entities/user.py
+++ back/src/model/entities/user.py
@@ -1,10 +1,10 @@
 from src.model.configs.base import Base
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, CHAR, String, Boolean
 from uuid import uuid4
 
 class User(Base):
     __tablename__ = "Usuario"
-    id = Column(String(36), primary_key=True, default=lambda: str(uuid4()))
+    id = Column(CHAR(36), primary_key=True, default=lambda: str(uuid4()))
     nome = Column(String(50))
     email = Column(String(50), unique=True)
     senha = Column(String(12))
