6043fef877a92fe36d8ea91ce14abed2a77fe2fe
Joselaine Romão <55799037+joselainejrs@users.noreply.github.com>
2025-04-08 14:08:29 -0300

    ajuste de home e retirada de package

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Menu from "../components/Menu";
 import ListagemEndereco from "../components/ListagemEndereco";
 import { decodeToken } from "../utils/decodeToken";
+import CardRestaurante from "../components/CardRestaurante";
 
 export default function Home() {
   const token = localStorage.getItem("token");
@@ -11,6 +12,18 @@ export default function Home() {
       <Menu>
         <ListagemEndereco idUsuario={idUsuario} />
       </Menu>
+
+      <div className="mt-[5rem]" >
+        <h1 className="my-8 text-center font-medium">Conheça os restaurantes disponíveis</h1>
+        <div className="flex gap-8 justify-center mt-[5rem]">
+          <CardRestaurante />
+          <CardRestaurante />
+          <CardRestaurante />
+          <CardRestaurante />
+          <CardRestaurante />
+        </div>
+
+      </div>
     </>
   );
 }
