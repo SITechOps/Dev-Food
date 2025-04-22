df925248897751b824ea30cb12e21c71ec9ca8b8
Enzoka123 <contato.enzo.mikami@gmail.com>
2025-04-21 21:23:12 -0300

    feat: integrar os dados dos produtos do servidor para o front

--- front/src/pages/RestaurantesDisponiveis/ProdutoDisponiveis/Index.tsx
+++ front/src/pages/RestaurantesDisponiveis/ProdutoDisponiveis/Index.tsx
@@ -1,76 +1,67 @@
 import { Minus, Plus } from "lucide-react";
 import Button from "../../../components/ui/Button";
-import { IProduto } from "../../../interface/IProduct";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { IRestaurante } from "../../../interface/IRestaurante";
 import { useCardProdutos } from "../../../hooks/useCardProduto";
-interface CardProdutosProps extends IProduto {
-	dadosRestaurante: IRestaurante;
-}
+import { CardProdutosProps } from "../../../interface/IProduct";
+
 
 export default function CardProdutos(props: CardProdutosProps) {
-	const {
-	  nome,
-	  descricao,
-	  imageUrl,
-	  valor_unitario,
-	  dadosRestaurante,
-	} = props;
-  
-	const {
-	  quantidade,
-	  incrementar,
-	  decrementar,
-	  adicionarAoCarrinho,
-	} = useCardProdutos(props, dadosRestaurante);
+  const { nome, descricao, imageUrl, valor_unitario, dadosRestaurante } = props;
 
+  const num = +valor_unitario;
 
-	return (
-		<div
-			className="w-full max-w-full rounded-md border border-gray-medium bg-gray-light p-4 transition-all duration-300 cursor-pointer"
-		>
-			<div className="flex gap-4">
-				<img
-					className="ml-2 mr-2 w-15 h-15 object-cover rounded-full mb-4 mx-auto"
-					src={imageUrl}
-					alt={nome}
-				/>
-				<h3 className="font-bold">{nome}</h3>
-			</div>
+  const { quantidade, incrementar, decrementar, adicionarAoCarrinho } =
+    useCardProdutos(props, dadosRestaurante);
 
-			<div className="w-full">
-				<p className="mt-2 font-light">{descricao}</p>
-				<p className="mt-3 text-[1.5rem] font-semibold">R$ {valor_unitario}</p>
+  return (
+    <div className="border-gray-medium bg-gray-light w-full max-w-full cursor-pointer rounded-md border p-4 transition-all duration-300">
+      <div className="flex gap-4">
+        <img
+          className="mx-auto mr-2 mb-4 ml-2 h-15 w-15 rounded-full object-cover"
+          src={
+            imageUrl ||
+            "https://food-guide.canada.ca/sites/default/files/styles/square_400_x_400/public/2020-12/CFGPlate-crop400x400.jpg"
+          }
+          alt={nome}
+        />
+        <h3 className="font-bold">{nome}</h3>
+      </div>
 
-				<div className="">
-					<div className="flex flex-row gap-4 items-center w-full justify-end">
-						<div className="flex gap-6 p-2 items-center justify-between rounded-lg border border-gray-medium bg-white">
-							<Button color="plain" onClick={decrementar} className="p-1">
-								<Minus size={20} className="stroke-2" />
-							</Button>
-							<span className="text-lg font-bold">{quantidade}</span>
-							<Button color="plain" onClick={incrementar} className="p-1">
-								<Plus size={20} className="stroke-2" />
-							</Button>
-						</div>
+      <div className="w-full">
+        <p className="mt-2 font-light">{descricao}</p>
+        <p className="mt-3 text-[1.5rem] font-semibold">R$ {valor_unitario}</p>
 
-						<Button
-							className="p-3 flex gap-6 items-center justify-between w-full md:w-auto"
-							disabled={quantidade === 0}
-							onClick={adicionarAoCarrinho}
-						>
-							<div className="flex items-center gap-2">
-								<AiOutlineShoppingCart className="text-2x1" />
-								<p className="text-sm md:text-base">Adicionar</p>
-							</div>
-							<span className="text-sm md:text-base">
-								R$ {quantidade > 0 ? (valor_unitario * quantidade).toFixed(2) : valor_unitario.toFixed(2)}
-							</span>
-						</Button>
-					</div>
-				</div>
-			</div>
-		</div>
+        <div className="">
+          <div className="flex w-full flex-row items-center justify-end gap-4">
+            <div className="border-gray-medium flex items-center justify-between gap-6 rounded-lg border bg-white p-2">
+              <Button color="plain" onClick={decrementar} className="p-1">
+                <Minus size={20} className="stroke-2" />
+              </Button>
+              <span className="text-lg font-bold">{quantidade}</span>
+              <Button color="plain" onClick={incrementar} className="p-1">
+                <Plus size={20} className="stroke-2" />
+              </Button>
+            </div>
 
-	);
+            <Button
+              className="flex w-full items-center justify-between gap-6 p-3 md:w-auto"
+              disabled={quantidade === 0}
+              onClick={adicionarAoCarrinho}
+            >
+              <div className="flex items-center gap-2">
+                <AiOutlineShoppingCart className="text-2x1" />
+                <p className="text-sm md:text-base">Adicionar</p>
+              </div>
+              <span className="text-sm md:text-base">
+                R${" "}
+                {quantidade > 0
+                  ? (num * quantidade).toFixed(2)
+                  : num.toFixed(2)}
+              </span>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
