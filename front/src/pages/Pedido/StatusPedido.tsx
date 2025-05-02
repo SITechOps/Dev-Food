719b735f97d8edfbad4f4fcea8eb003191de1c41
Gustavo <gustrpaz@gmail.com>
2025-05-01 22:23:18 -0300

    refactor: Refatorar espaçamento entre pedidos status

--- front/src/pages/Pedido/StatusPedido.tsx
+++ front/src/pages/Pedido/StatusPedido.tsx
@@ -73,66 +73,73 @@ export default function OrderStatusTracker() {
           Nenhum pedido em andamento.
         </p>
       ) : (
-        pedidosEmAberto.map((pedido) => {
-          const step = statusSteps[pedido.status] ?? 0;
-          const steps = 4;
+        <div className="flex flex-col gap-6">
+          {pedidosEmAberto.map((pedido) => {
+            const step = statusSteps[pedido.status] ?? 0;
+            const steps = 4;
 
-          return (
-            <div
-              key={pedido.Id}
-              className="border-gray-lightmb-8 rounded-xl border bg-white p-4 shadow-sm"
-            >
-              <h3 className="text-blue text-lg font-semibold">
-                Acompanhamento do pedido
-              </h3>
-              <div className="my-3 flex w-full items-center justify-between">
-                <div>
-                  <span className="text-gray-medium">Previsão de entrega:</span>
-                  <p className="text-blue text-md mb-2 font-bold">
-                    {formatarData(
-                      acrescentarHora(pedido.data_pedido).toISOString(),
-                    )}
+            return (
+              <div
+                key={pedido.Id}
+                className="border-gray-light rounded-xl border bg-white p-4 shadow-sm"
+              >
+                <h3 className="text-blue text-lg font-semibold">
+                  Acompanhamento do pedido
+                </h3>
+                <div className="my-3 flex w-full items-center justify-between">
+                  <div>
+                    <span className="text-gray-medium">
+                      Previsão de entrega:
+                    </span>
+                    <p className="text-blue text-md mb-2 font-bold">
+                      {formatarData(
+                        acrescentarHora(pedido.data_pedido).toISOString(),
+                      )}
+                    </p>
+                  </div>
+                  <span className="text-gray-medium font-medium">
+                    Atualização em tempo real
+                  </span>
+                </div>
+
+                <div className="mb-2 flex items-center justify-between">
+                  {[...Array(steps)].map((_, index) => (
+                    <div
+                      key={index}
+                      className={`mx-1 h-1 flex-1 rounded-full ${
+                        index <= step ? "bg-blue" : "bg-gray-medium"
+                      }`}
+                    />
+                  ))}
+                </div>
+
+                <div className="text-blue mt-4 mb-4 ml-1 flex items-center gap-2">
+                  <span className="relative flex h-2 w-2">
+                    <span className="bg-brown-normal absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"></span>
+                    <span className="bg-brown-normal relative inline-flex h-2 w-2 rounded-full"></span>
+                  </span>
+                  <p>
+                    {statusMessages[pedido.status] ?? "Status desconhecido"}
                   </p>
                 </div>
-                <span className="text-gray-medium font-medium">
-                  Atualização em tempo real
-                </span>
-              </div>
 
-              <div className="mb-2 flex items-center justify-between">
-                {[...Array(steps)].map((_, index) => (
+                <div className="flex w-full justify-center">
                   <div
-                    key={index}
-                    className={`mx-1 h-1 flex-1 rounded-full ${
-                      index <= step ? "bg-blue" : "bg-gray-medium"
-                    }`}
-                  />
-                ))}
-              </div>
-
-              <div className="text-blue mt-4 mb-4 ml-1 flex items-center gap-2">
-                <span className="relative flex h-2 w-2">
-                  <span className="bg-brown-normal absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"></span>
-                  <span className="bg-brown-normal relative inline-flex h-2 w-2 rounded-full"></span>
-                </span>
-                <p>{statusMessages[pedido.status] ?? "Status desconhecido"}</p>
-              </div>
-              <div className="flex w-full justify-center">
-                <div
-                  className="border-brown-normal mb-4 flex w-fit flex-col gap-2 border px-10 py-3 text-center"
-                  style={{ borderRadius: "10px" }}
-                >
-                  <span className="text-gray-medium">
-                    Código de confirmação de entrega
-                  </span>
-                  <span className="text-blue text-2xl font-semibold">
-                    {getCodigoPedido(pedido.Id)}
-                  </span>
+                    className="border-brown-normal mb-4 flex w-fit flex-col gap-2 border px-10 py-3 text-center"
+                    style={{ borderRadius: "10px" }}
+                  >
+                    <span className="text-gray-medium">
+                      Código de confirmação de entrega
+                    </span>
+                    <span className="text-blue text-2xl font-semibold">
+                      {getCodigoPedido(pedido.Id)}
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })
+            );
+          })}
+        </div>
       )}
     </div>
   );
