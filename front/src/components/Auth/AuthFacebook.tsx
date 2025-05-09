992131792227bcb9a3f327df1a60505ece040418
RodrigoMazucato <rodrigolopes.sp2005@gmail.com>
2025-05-08 22:33:10 -0300

    fix: corrigir erros de tipagem e mover APP_ID para o .env

--- front/src/components/Auth/AuthFacebook.tsx
+++ front/src/components/Auth/AuthFacebook.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import { useAuthUserComponent } from "../../hooks/useAuthUser";
 import Button from "../ui/Button";
@@ -13,7 +12,6 @@ interface AuthFacebookProps {
 }
 
 function AuthFacebook({ setEtapa, setFormList }: AuthFacebookProps) {
-  const navigate = useNavigate();
   const { loginUser } = useAuthUserComponent();
   const { setAuth } = useAuth();
   const [user, setUser] = useState<any>(null); // Estado para armazenar os dados do usuário do Facebook
@@ -23,50 +21,58 @@ function AuthFacebook({ setEtapa, setFormList }: AuthFacebookProps) {
 
     const { email } = user;
     (async () => {
-        try {
-          await loginUser(email);
-        } catch {
-          setFormList((prev) => ({ ...prev, email }));
-          setEtapa("telefone");
-        }
-      })();
-  },[user, setAuth])
+      try {
+        await loginUser(email);
+      } catch {
+        setFormList((prev) => ({ ...prev, email }));
+        setEtapa("telefone");
+      }
+    })();
+  }, [user, setAuth]);
 
   useEffect(() => {
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: '1359659571817427',
+    (window as any).fbAsyncInit = function () {
+      (window as any).FB.init({
+        appId: import.meta.env.VITE_FACEBOOK_APP_ID,
         cookie: true,
         xfbml: true,
-        version: 'v22.0'
+        version: "v22.0",
       });
 
-      window.FB.AppEvents.logPageView();
+      (window as any).FB.AppEvents.logPageView();
     };
-
     (function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) { return; }
-      js = d.createElement(s); js.id = id;
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s) as HTMLScriptElement;
+      js.id = id;
       js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+      fjs.parentNode?.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
   }, []); // Executa apenas uma vez na montagem do componente
 
   function loginComFacebook() {
-    window.FB.login(response => {
-      if (response.authResponse) {
-        console.log('Welcome! Fetching your information.... ');
-        window.FB.api('/me', { fields: 'email' }, userInfo => {
-          console.log('Usuário logado:', userInfo);
-          setUser(userInfo); // Atualiza o estado 'user' com os dados do usuário
-        });
-      } else {
-        console.log('User cancelled login or did not fully authorize.');
-      }
-    }, { scope: 'email' });
-  };
-
+    (window as any).FB.login(
+      (response: { authResponse: any }) => {
+        if (response.authResponse) {
+          (window as any).FB.api(
+            "/me",
+            { fields: "email" },
+            (userInfo: any) => {
+              console.log("Usuário logado:", userInfo.email);
+              setUser(userInfo); // Atualiza o estado 'user' com os dados do usuário
+            },
+          );
+        } else {
+          console.log("User cancelled login or did not fully authorize.");
+        }
+      },
+      { scope: "email" },
+    );
+  }
   return (
     <Button
       className="bg-blue-dark hover:bg-blue flex items-center justify-center gap-2 p-2"
