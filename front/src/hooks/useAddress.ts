0701b141bd0598e0cbb97f5e266f561f20a651bf
Gustavo <gustrpaz@gmail.com>
2025-05-12 22:17:34 -0300

    chore: Corrigir erros de ts

--- front/src/hooks/useAddress.ts
+++ front/src/hooks/useAddress.ts
@@ -4,7 +4,7 @@ import { reverseGeoCode } from "../utils/geolocation";
 import { api } from "../connection/axios";
 import { useAuth } from "../contexts/AuthContext";
 import { IEndereco } from "../interface/IEndereco";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { initMapScript } from "@/utils/initMapScript";
 
 export const useEndereco = () => {
@@ -128,7 +128,6 @@ export const useEndereco = () => {
       return alert("Por favor, preencha todos os campos do endereço.");
     if (!idUsuario) return alert("Erro ao obter ID do usuário.");
 
-    const enderecoId = await getEnderecoId();
     if (!enderecoId) return alert("Endereço não encontrado para edição.");
 
     try {
